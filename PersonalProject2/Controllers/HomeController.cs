8266961be4dbe0f021c1f3b4dd7278f7fa4ea5e0
kellilee7398 <kellilee73@gmail.com>
2021-10-20 08:59:25 -0500

    Add project files.

---
+++ PersonalProject2/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PersonalProject2.Models;
+
+namespace PersonalProject2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        public ViewResult Login(ResponseEntry responseEntry)
+        {
+            Respository.AddResponse(responseEntry);
+            return View();
+        }
+        
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PersonalProject2/Models/Respository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalProject2.Models
+{
+    public class Respository
+    {
+        public static List<ResponseEntry> responseEntries = new List<ResponseEntry>();
+        public static IEnumerable<ResponseEntry> ResponseEntries
+        {
+            get { return responseEntries; }
+        }
+
+        public static void AddResponse(ResponseEntry responseEntry)
+        {
+            responseEntries.Add(responseEntry);
+        }
+    }
+}
